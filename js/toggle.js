5dda3dfa867bdcff1ed09c0606a41b3ca06a9ef4
areenatanim <areenatanim@gmail.com>
2026-04-13 12:28:17 +0600

    toggle.js page added

---
+++ js/toggle.js
@@ -0,0 +1,17 @@
+// showOnly function for toggle
+
+
+function showOnly(id) {
+        const interview = document.getElementById("interview");
+        const reject = document.getElementById("reject");
+        const cardSection = document.getElementById("card-section");
+        interview.classList.add("hidden");
+        reject.classList.add("hidden");
+        cardSection.classList.add("hidden")
+
+        const selected = document.getElementById(id);
+        selected.classList.remove("hidden");
+
+
+
+}
